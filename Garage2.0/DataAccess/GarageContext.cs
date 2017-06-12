687a50ad32f2919a024e40a952078740e864bcf9
User <User@172GJ32>
2017-06-12 16:49:41 +0200

    DataConnection changed to Garage2.0

--- Garage2.0/DataAccess/GarageContext.cs
+++ Garage2.0/DataAccess/GarageContext.cs
@@ -8,7 +8,7 @@ namespace Garage2._0.DataAccess
 {
     public class GarageContext : DbContext
     {
-        public GarageContext() : base("DefaultConnection")
+        public GarageContext() : base("Garage2.0")
         {
 
         }
