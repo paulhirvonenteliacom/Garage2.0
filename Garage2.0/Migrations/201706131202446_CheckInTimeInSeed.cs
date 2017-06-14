2cee7ee44d8a0f19426e75aba85b17072cf07a99
User <User@172GJ32>
2017-06-14 08:32:50 +0200

    Add migrations or parking details

---
+++ Garage2.0/Migrations/201706131202446_CheckInTimeInSeed.cs
@@ -0,0 +1,18 @@
+namespace Garage2._0.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CheckInTimeInSeed : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.ParkedVehicles", "CheckInTime", c => c.DateTime(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.ParkedVehicles", "CheckInTime");
+        }
+    }
+}
--- Garage2.0/Migrations/Configuration.cs
+++ Garage2.0/Migrations/Configuration.cs
@@ -18,13 +18,13 @@ namespace Garage2._0.Migrations
             context.ParkedVehicles.AddOrUpdate(
 
                 pv => pv.RegNumber,
-                new Models.ParkedVehicle { TypeOfVehicle =  Car, RegNumber = "QSW098", Brand = "BMW", Model = "F800R", Color = "Black", NoOfWheels = 4 },
-                new Models.ParkedVehicle { TypeOfVehicle = Car, RegNumber = "EDC321", Brand = "FIAT", Model = "500L", Color = "Yellow", NoOfWheels = 4 },
-                new Models.ParkedVehicle { TypeOfVehicle = Car, RegNumber = "AQR564", Brand = "VW", Model = "Passat", Color = "Silver", NoOfWheels = 4 },
+                new Models.ParkedVehicle { TypeOfVehicle =  Car, RegNumber = "QSW098", Brand = "BMW", Model = "F800R", Color = "Black", NoOfWheels = 4, CheckInTime = DateTime.Now },
+                new Models.ParkedVehicle { TypeOfVehicle = Car, RegNumber = "EDC321", Brand = "FIAT", Model = "500L", Color = "Yellow", NoOfWheels = 4, CheckInTime = DateTime.Now },
+                new Models.ParkedVehicle { TypeOfVehicle = Car, RegNumber = "AQR564", Brand = "VW", Model = "Passat", Color = "Silver", NoOfWheels = 4, CheckInTime = DateTime.Now },
 
-                new Models.ParkedVehicle { TypeOfVehicle = Bus, RegNumber = "DSL675", Brand = "Scania", Model = "DC13", Color = "White", NoOfWheels = 6 },
-                new Models.ParkedVehicle { TypeOfVehicle = Boat, RegNumber = "ADKN2592", Brand = "Home", Model = "F800R", Color = "Blue", NoOfWheels = 0 },
-                new Models.ParkedVehicle { TypeOfVehicle = Airplane, RegNumber = "NAB810BW", Brand = "Cessna", Model = "402", Color = "White", NoOfWheels = 4}
+                new Models.ParkedVehicle { TypeOfVehicle = Bus, RegNumber = "DSL675", Brand = "Scania", Model = "DC13", Color = "White", NoOfWheels = 6, CheckInTime = DateTime.Now },
+                new Models.ParkedVehicle { TypeOfVehicle = Boat, RegNumber = "ADKN2592", Brand = "Home", Model = "F800R", Color = "Blue", NoOfWheels = 0, CheckInTime = DateTime.Now },
+                new Models.ParkedVehicle { TypeOfVehicle = Airplane, RegNumber = "NAB810BW", Brand = "Cessna", Model = "402", Color = "White", NoOfWheels = 4, CheckInTime = DateTime.Now }
                 );
         }
 
